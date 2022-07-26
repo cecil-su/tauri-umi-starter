d3b15a258284928e2cff90bbd9935157afcce2e1
cecil-su <cecil_su@163.com>
2022-07-26 15:59:41 +0800

    test actions workflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import yayJpg from '../assets/yay.jpg';
 export default function HomePage() {
   return (
     <div>
-      <h2>Yay! Welcome to umi!</h2>
+      <h2>Yay! Welcome to Tauri umi starter!</h2>
       <p>
         <img src={yayJpg} width="388" />
       </p>
